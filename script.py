c0d79c51b663bcbdeabfd9b166fda097ad61fe0f
Lucy Gavrich <annalgav@gmail.com>
2023-02-23 10:50:42 -0800

    Shorten line lengths for readability

--- script.py
+++ script.py
@@ -18,7 +18,10 @@ def create_movie_dict() -> defaultdict:
 
     for i in range(len(movie_data)):
         year = movie_data['title_year'][i]
-        title = unicodedata.normalize('NFKD', movie_data['movie_title'][i]).strip()
+        title = unicodedata.normalize(
+            'NFKD',
+            movie_data['movie_title'][i],
+        ).strip()
         movie_dict[str(round(year))].append(title)
 
     return movie_dict
@@ -30,8 +33,12 @@ def sort_years(movie_dict: defaultdict) -> List[str]:
     return sorted(movie_dict.keys())
 
 
-def get_possible_years(movie_dict: defaultdict, sorted_years: List[str], input_str: str) -> List[str]:
-    """Get list of possible release years from user input of partial or complete year string."""
+def get_possible_years(
+        movie_dict: defaultdict,
+        sorted_years: List[str],
+        input_str: str,
+    ) -> List[str]:
+    """Get list of possible release years from user input of year string."""
 
     input_len = len(input_str)
 
@@ -46,7 +53,8 @@ def get_possible_years(movie_dict: defaultdict, sorted_years: List[str], input_s
         for i in range(len(sorted_years)):
             if sorted_years[i][:input_len] == input_str:
                 curr_idx = i
-                while curr_idx < len(sorted_years) and sorted_years[curr_idx][:input_len] == input_str:
+                while (curr_idx < len(sorted_years)
+                       and sorted_years[curr_idx][:input_len] == input_str):
                     possible_years.append(sorted_years[curr_idx])
                     curr_idx += 1
                 break
@@ -73,35 +81,56 @@ def recommend(movie_dict: defaultdict) -> None:
     rec = True
 
     while rec:
-        input_str = input('Type the beginning of the year you would like to see: ')
+        input_str = input(
+            'Type the beginning of the year you would like to see: '
+        )
         if len(input_str) > 4:
             print('Year must be 4 digits or less')
             continue
 
         else:
-            possible_years = get_possible_years(movie_dict, sort_years(movie_dict), input_str)
+            possible_years = get_possible_years(
+                movie_dict,
+                sort_years(movie_dict),
+                input_str,
+            )
 
             if not possible_years:
-                print('We don\'t have any recommendations for that year, please try another')
+                print(''.join([
+                    'We don\'t have any recommendations ',
+                    'for that year, please try another',
+                ]))
                 continue
 
             elif len(possible_years) == 1:
                 if len(input_str) < 4:
-                    print(f'There is one year in our system that matches your request: {possible_years[0]}')
-                    is_desired = input('Would you like to see movies for this year? [y/n]: ').lower()
+                    print(''.join([
+                        'There is one year in our system that matches your ',
+                        'request: ',
+                        possible_years[0],
+                    ]))
+                    is_desired = input(
+                        'Would you like to see movies for this year? [y/n]: '
+                    ).lower()
 
                     if is_desired != 'y':
                         continue
 
-                print(f'Here are some movies that were released in {possible_years[0]}')
+                print(''.join([
+                    'Here are some movies that were released in ',
+                    possible_years[0],
+                ]))
                 display_movies(movie_dict, possible_years[0])
 
-                see_more = input('Would you like to search for a different year? [y/n]: ').lower()
+                see_more = input(
+                    'Would you like to search for a different year? [y/n]: '
+                ).lower()
                 if see_more != 'y':
                     break
 
             else:
-                print(f'Here are some years that match your request:\n {possible_years}')
+                print('Here are some years that match your request:\n')
+                print(possible_years)
 
     print('Thank you for using Movie Recommendations by Release Year')
 
